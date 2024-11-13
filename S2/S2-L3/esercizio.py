d4232ac955229b5b624a409542b5bdeac0c7ebdd
Matteo1606 <matteo.maglione16@gmail.com>
2024-11-13 15:55:55 +0100

    esercizio  python

--- S2/S2-L3/esercizio.py
+++
@@ -1,1 +0,0 @@
-citta = input
---
+++ esercizio.py
@@ -0,0 +1,4 @@
+citta=input ("dimmi la tua citta' di origine:   ")
+animale=input ("dimmi il nome del tuo animale domestico:   ")
+
+print(f"il nome della tua band potrebbe essere: {animale}{citta}")
