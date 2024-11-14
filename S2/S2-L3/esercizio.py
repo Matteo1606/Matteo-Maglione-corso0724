d845b166da5f668eb41d715c5dc033fb341c0836
Matteo1606 <matteo.maglione16@gmail.com>
2024-11-14 17:38:10 +0100

    esercizio perimetro

--- S2/S2-L3/esercizio.py
+++ S2/S2-L3/esercizio.py
@@ -1,4 +1,5 @@
 citta=input ("dimmi la tua citta' di origine:   ")
 animale=input ("dimmi il nome del tuo animale domestico:   ")
 
+
 print(f"il nome della tua band potrebbe essere: {animale}{citta}")
---
+++ esercizio2.py
@@ -0,0 +1,33 @@
+import math
+
+def perimetro_quadrato(lato):
+    quadrato=perimetro_quadrato
+    return lato * 4
+def perimetro_cerchio(raggio):
+    cerchio=perimetro_cerchio
+    return math.pi * raggio * 2
+def perimetro_rettangolo(lunghezza, larghezza):
+    rettangolo=perimetro_rettangolo
+    return (lunghezza + larghezza) * 2
+
+
+scelta=input("Scegli di quale figura geometrica vuoi calcolare il perimetro (quadrato, cerchio, rettangolo):   ")
+
+if scelta == "quadrato":
+    lato = float(input("inserisci la lunghezza del lato:  "))
+    perimetro=perimetro_quadrato(lato)
+    print(f"il perimetro del quadrato è:  {lato * 4}")
+
+elif scelta == "cerchio":
+    raggio = float(input("Inserisci il raggio del cerchio:  "))
+    perimetro=perimetro_cerchio
+    print(f"il perimetro del cerchio è:  {math.pi * raggio * 2}")
+
+elif scelta == "rettangolo":
+    lunghezza = float(input("inserisci la lunghezza del rettangolo:  "))
+    larghezza = float(input("inserici la larghezza del rettangolo:  "))
+    perimetro=perimetro_rettangolo
+    print(f"il perimetro del rettangolo è:  {(lunghezza + larghezza) * 2}")
+
+else:
+    print("penso proprio che dovresti prenotare una visita dall'oculista...")
